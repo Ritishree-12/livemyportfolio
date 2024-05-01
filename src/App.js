b58bd93da2ca649db7a01cfb41521f50e6934144
Ritishree-12 <Ritishree1996@gmail.com>
2024-05-01 11:59:09 +0530

    added image

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
      <Header/>
       <Routes>      
-          <Route path='/' element={<Home/>} />
+          <Route path='/Home' element={<Home/>} />
           {/* <Route path='/' element={<Home/>} /> */}
           <Route path='/about' element={<About />} />
           <Route path="/contact" element={<Contact/>} />
