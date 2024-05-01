77a7b194e5515b14d6319fd66f7f5725e7847c99
Ritishree-12 <Ritishree1996@gmail.com>
2024-05-01 13:11:56 +0530

    added image

--- src/App.js
+++ src/App.js
@@ -2,31 +2,34 @@ import './App.css';
 import './style.css'
 import About from './components/Pages/About';
 import Contact from './components/Pages/Contact';
-import Footer from './components/footer/Footer';
+// import Footer from './components/footer/Footer';
 import Home from './components/Pages/Home';
 import Projects from './components/Pages/Projects';
 import Resume from './components/Pages/Resume';
-import Header from './components/header/Header';
+// import Header from './components/header/Header';
 import Educational from './components/Pages/Educational';
 import { Routes, Route } from "react-router-dom";
+import Welcome from './components/Pages/Welcome';
 
 function App() {
   return (
-    
+
     <div className="App">
-     <Header/>
-      <Routes>      
-          <Route path='/' element={<Home/>} />
-          {/* <Route path='/' element={<Home/>} /> */}
-          <Route path='/about' element={<About />} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/projects" element={<Projects/>} />
-          <Route path="/resume" element={<Resume/>} />
-          <Route path="/educational" element={<Educational/>} />     
+      {/* <Header /> */}
+      {/* <Welcome /> */}
+      <Routes>
+      <Route path='/' element={<Welcome/>} />
+        <Route path='/home' element={<Home />} />
+        {/* <Route path='/' element={<Home/>} /> */}
+        <Route path='/about' element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/educational" element={<Educational />} />
       </Routes>
-      <Footer/>
+      {/* <Footer /> */}
     </div>
-    
+
   );
 }
 
